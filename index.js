00758e117887bd9301b10c15f837dbbb81886f8f
kozaku05 <kozaku0505@gmail.com>
2025-01-19 22:36:22 +0900

    修正

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ let page;
 let promise = false;
 const [x, y, width, height] = [200, 340, 380, 35];
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  page = await browser.newPage();
-  page.on("close", () => {
-    process.exit(0);
-  });
-  await page.goto("https://sushida.net/play.html");
-  start();
-  async function start() {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+    await page.goto("https://sushida.net/play.html");
+  } catch (error) {
+    console.error(chalk.red("ブラウザが起動できませんでした:"));
+    process.exit(1);
+  }
+
+  async function tool() {
     await new Promise(async (re) => {
       console.log(
         chalk.red("\n注意") +
@@ -31,44 +33,58 @@ const [x, y, width, height] = [200, 340, 380, 35];
         chalk.green("準備ができたらエンターで開始-exitで終了: "),
         async (text) => {
           if (text == "exit") {
-            await browser.close();
             process.exit(0);
           }
           re();
         }
       );
     });
-    const loop = setInterval(shot, 300);
+    const loop = setInterval(startloop, 300);
     rl.question("", () => {
       clearInterval(loop);
       setTimeout(() => {
-        start();
+        tool();
       }, 1000);
     });
   }
+  tool();
+  page.on("close", () => {
+    process.exit(0);
+  });
 })();
-async function shot() {
+async function startloop() {
   if (promise) return;
   promise = true;
-  const screenshot = await page.screenshot({
-    clip: {
-      x: x,
-      y: y,
-      width: width,
-      height: height,
-    },
-  });
-  const {
-    data: { text },
-  } = await Tesseract.recognize(screenshot, "eng");
-  if (!text) return (promise = false);
-  let result = text.match(/[a-zA-Z-/?!]+/g);
-  if (result.length > 1) {
-    result = result.filter((t) => t.length > 3);
+  let screenshot;
+  try {
+    screenshot = await page.screenshot({
+      clip: {
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+      },
+    });
+  } catch (error) {
+    console.log(chalk.yellow("スクリーンショットの取得に失敗しました"));
+    return (promise = false);
   }
-  console.log("[" + chalk.blue(result) + "]");
-  if (result) {
-    await page.keyboard.type(result.join(""));
+  try {
+    const {
+      data: { text },
+    } = await Tesseract.recognize(screenshot, "eng");
+    if (!text) return (promise = false);
+    let result = text.match(/[a-zA-Z-/?!]+/g);
+    if (result.length > 1) {
+      result = result.filter((t) => t.length > 3);
+    }
+    console.log("[" + chalk.blue(result) + "]");
+    if (result) {
+      await page.keyboard.type(result.join(""));
+    }
+    promise = false;
+  } catch (error) {
+    console.log(chalk.yellow("文字認識に失敗しました。"));
+    promise = false;
   }
-  promise = false;
 }
